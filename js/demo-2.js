c5351fd35f44f1fde46acde7b9224b87047b1cdb
zephyr-art <79492082+zephyr-art@users.noreply.github.com>
2021-02-23 10:51:57 -0500

    Update demo-2.js

--- js/demo-2.js
+++ js/demo-2.js
@@ -8,7 +8,7 @@
 
     function initHeader() {
         width = window.innerWidth;
-        height = 900;
+        height = 1020;
         target = {x: 0, y: height};
 
         largeHeader = document.getElementById('large-header');
